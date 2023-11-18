c2c4b3a816a619a96b27a33aba0d3c1c08c89cef
Thênh Thang <quocntrivn@ymail.com>
2023-11-18 10:10:49 +0700

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sleep 1 && curl -s https://raw.githubusercontent.com/thenhthang/vinnodes/main/logo.sh | bash && sleep 1
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating dependencies... \e[0m" && sleep 1
+
+sudo apt-get update
+
+echo "=================================================="
+echo -e "\e[1m\e[32m2. Installing required dependencies... \e[0m" && sleep 1
+
+sudo apt install jq -y
+sudo apt install python3-pip -y
+sudo pip install yq
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m3. Checking if Docker is installed... \e[0m" && sleep 1
+
+if ! command -v docker &> /dev/null
+then
+    echo -e "\e[1m\e[32m3.1 Installing Docker... \e[0m" && sleep 1
+    sudo apt-get install ca-certificates curl gnupg lsb-release wget -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    sudo chmod a+r /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+fi
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m4. Checking if Docker Compose is installed ... \e[0m" && sleep 1
+
+docker-compose version
+if [ $? -ne 0 ]
+then
+    echo -e "\e[1m\e[32m4.1 Installing Docker Compose... \e[0m" && sleep 1
+	docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
+	sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+	sudo chmod +x /usr/bin/docker-compose
+fi
+echo "=================================================="
+echo -e "\e[1m\e[32m5. Installation is complete \e[0m" && sleep 1
