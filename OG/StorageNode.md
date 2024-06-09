a98ea938576c3d439a36f5db875e3b2b17b28f6c
Thênh Thang <quocntrivn@ymail.com>
2024-06-09 11:38:51 +0000

    files added

---
+++ OG/StorageNode.md
@@ -0,0 +1,57 @@
+<p style="font-size:14px" align="right">
+<a href="https://t.me/nodesrunnerguru" target="_blank">Join our telegram <img src="https://user-images.githubusercontent.com/50621007/183283867-56b4d69f-bc6e-4939-b00a-72aa019d1aea.png" width="30"/></a>
+<a href="https://discord.gg/dvNSrwyU" target="_blank">Join our discord <img src="https://user-images.githubusercontent.com/50621007/176236430-53b0f4de-41ff-41f7-92a1-4233890a90c8.png" width="30"/></a>
+<a href="https://vinnodes.com" target="_blank">Visit our website <img src="https://github.com/thenhthang/vinnodes/blob/main/content/logo.jpg?raw=true" width="30"/></a>
+</p>
+<p align="center">
+  <img height="100" height="auto" src="https://github.com/thenhthang/vinnodes/blob/main/content/oglogo.png?raw=true">
+</p>
+
+# OG Storage Node
+
+Official documentation:
+>- https://docs.0g.ai/0g-doc/run-a-node/storage-node-and-da-services
+
+Faucet:
+>- https://faucet.0g.ai
+
+Submit Google form:
+>- https://docs.google.com/forms/d/e/1FAIpQLScsa1lpn43F7XAydVlKK_ItLGOkuz2fBmQaZjecDn76kysQsw/viewform?ts=6617a343
+
+## Hardware Requirements 
+Minimum
+>- 16GB RAM
+>- 4Core CPU
+>- Disk: 500G NVME SSD
+>- Bandwidth: 600 MBps for Download / Upload
+
+| Network | Version | Current | Last modified |
+|---------------|-------------|-------------|-------------|
+| **zgtendermint_16600-1** | v0.2.0 | Yes |  |
+
+# You can setup your OG Storage Node in few minutes by using automated script below.
+## It will prompt you to input your EVM PRIVATE KEY and your BLOCKCHAIN RPC ENDPOIT!
+## Get evm private key
+Open metamask wallet > Account details > Show private key
+## List rpc endpoint
+You can use your own rpc end point or use any rpc below
+- https://rpc-testnet.0g.ai
+- https://evm-rpc-0g.mflow.tech
+- https://0g-rpc.dongqn.com
+- https://og-testnet-jsonrpc.blockhub.id
+## Please rpc is work or not before install 
+```
+curl -X POST https://rpc-testnet.0g.ai -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+```
+If you see same, it's working
+![alt text](image.png)
+# Ok, let's go, setup storage node automatic with one command
+```
+wget -O ogstorage-auto.sh https://raw.githubusercontent.com/thenhthang/vinnodes/main/OG/ogstorage-auto.sh && chmod +x ogstorage-auto.sh && ./ogstorage-auto.sh
+```
+# Useful commands
+
+## check your log list
+ls ~/0g-storage-node/run/log/
+## check your last log
+tail -f ~/0g-storage-node/run/log/zgs.log.$(date +%Y-%m-%d)
---
+++ OG/ogstorage-auto.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+echo -e "\033[0;35m"
+echo " __      _______ _   _ _   _  ____  _____  ______  _____  ";
+echo " \ \    / /_   _| \ | | \ | |/ __ \|  __ \|  ____|/ ____| ";
+echo "  \ \  / /  | | |  \| |  \| | |  | | |  | | |__  | (___   ";
+echo "   \ \/ /   | | |     |     | |  | | |  | |  __|  \___ \  ";
+echo "    \  /   _| |_| |\  | |\  | |__| | |__| | |____ ____) | ";
+echo "     \/   |_____|_| \_|_| \_|\____/|_____/|______|_____/    ";
+echo -e "\e[0m"
+
+sleep 2
+
+# set vars
+if [ ! $PRIVATE_KEY ]; then
+	read -p "Enter your evm private key: " PRIVATE_KEY
+	echo 'export PRIVATE_KEY=$PRIVATE_KEY' >> $HOME/.bash_profile
+fi
+if [ ! $BLOCKCHAIN_RPC_ENDPOINT ]; then
+	read -p "Enter your BLOCKCHAIN_RPC_ENDPOINT: " BLOCKCHAIN_RPC_ENDPOINT
+	echo 'export BLOCKCHAIN_RPC_ENDPOINT=$BLOCKCHAIN_RPC_ENDPOINT' >> $HOME/.bash_profile
+fi
+echo 'export LOG_CONTRACT_ADDRESS="0xb8F03061969da6Ad38f0a4a9f8a86bE71dA3c8E7"' >> ~/.bash_profile
+echo 'export MINE_CONTRACT="0x96D90AAcb2D5Ab5C69c1c351B0a0F105aae490bE"' >> ~/.bash_profile
+echo 'export ZGS_LOG_SYNC_BLOCK="445493"' >> ~/.bash_profile
+source ~/.bash_profile
+echo -e "LOG_CONTRACT_ADDRESS: $LOG_CONTRACT_ADDRESS\nMINE_CONTRACT: $MINE_CONTRACT\nZGS_LOG_SYNC_BLOCK: $ZGS_LOG_SYNC_BLOCK\n\n\033[33mby Nodebrand.\033[0m"
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+
+sudo apt-get install clang cmake build-essential git
+
+# install go
+
+cd $HOME && \
+ver="1.22.0" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+source $HOME/.bash_profile && \
+go version
+sleep 1
+
+# install rust
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+
+# download binary
+
+git clone -b v0.2.0 https://github.com/0glabs/0g-storage-node.git
+
+# build
+cd $HOME/0g-storage-node
+git submodule update --init
+sudo apt install cargo
+cargo build --release
+
+# update config
+sed -i '
+s|# network_dir = "network"|network_dir = "network"|
+s|# network_enr_tcp_port = 1234|network_enr_tcp_port = 1234|
+s|# network_enr_udp_port = 1234|network_enr_udp_port = 1234|
+s|# network_libp2p_port = 1234|network_libp2p_port = 1234|
+s|# network_discovery_port = 1234|network_discovery_port = 1234|
+s|# rpc_enabled = true|rpc_enabled = true|
+s|# db_dir = "db"|db_dir = "db"|
+s|# log_config_file = "log_config"|log_config_file = "log_config"|
+s|# log_directory = "log"|log_directory = "log"|
+' $HOME/0g-storage-node/run/config.toml
+
+sed -i '
+s|^network_boot_nodes = \".*\"|network_boot_nodes = \["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS"\]|
+s|^log_contract_address = ".*"|log_contract_address = "'"$LOG_CONTRACT_ADDRESS"'"|
+s|^mine_contract_address = ".*"|mine_contract_address = "'"$MINE_CONTRACT"'"|
+s|^log_sync_start_block_number = .*|log_sync_start_block_number = '"$ZGS_LOG_SYNC_BLOCK"'|
+' $HOME/0g-storage-node/run/config.toml
+
+sed -i "s|^blockchain_rpc_endpoint = \".*\"|blockchain_rpc_endpoint = \"$BLOCKCHAIN_RPC_ENDPOINT\"|" $HOME/0g-storage-node/run/config.toml
+sed -i 's|^miner_key = ""|miner_key = "'"$PRIVATE_KEY"'"|' $HOME/0g-storage-node/run/config.toml
+
+
+# create service
+
+sudo tee /etc/systemd/system/zgs.service > /dev/null <<EOF
+[Unit]
+Description=ZGS Node
+After=network.target
+
+[Service]
+User=root
+WorkingDirectory=$HOME/0g-storage-node/run
+ExecStart=$HOME/0g-storage-node/target/release/zgs_node --config $HOME/0g-storage-node/run/config.toml
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# run
+
+sudo systemctl daemon-reload && \
+sudo systemctl enable zgs && \
+sudo systemctl start zgs
+
+echo -e "\e[1m\e[32m2. Install complete \e[0m" && sleep 1
