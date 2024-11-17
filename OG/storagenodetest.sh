e18c19984e44483ab8ac480ec5faf2675ca804c4
Thênh Thang <quocntrivn@ymail.com>
2024-11-17 13:25:23 +0700

    Update storagenodetest.sh

--- OG/storagenodetest.sh
+++ OG/storagenodetest.sh
@@ -2,6 +2,6 @@ while true; do
     response=$(curl -s -X POST http://localhost:5678 -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"zgs_getStatus","params":[],"id":1}')
     logSyncHeight=$(echo $response | jq '.result.logSyncHeight')
     connectedPeers=$(echo $response | jq '.result.connectedPeers')
-    echo -e "Block: \033[32m$logSyncHeight\033[0m, Peers: \033[34m$connectedPeers\033[0m"
+    echo -e "Last block height: \033[32m$logSyncHeight\033[0m, Connected peers: \033[34m$connectedPeers\033[0m"
     sleep 1; 
 done
